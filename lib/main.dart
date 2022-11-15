04f060c504ff3ed7630a9298ae0be4620ab894cb
Wong Re Jie <wrjie2000@gmail.com>
2022-11-15 14:05:53 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,152 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:rounded_loading_button/rounded_loading_button.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Movies City'),
+        ),
+        body: const HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController textSearch = TextEditingController();
+  //variables initialization
+  var movieTitle = "",
+      year = "",
+      genre = "",
+      imageUrl = "https://via.placeholder.com/250x300.png?text=No+Record",
+      desc = "";
+  final RoundedLoadingButtonController _btnController =
+      RoundedLoadingButtonController();
+
+  void _buttonLoading() async {
+    Timer(const Duration(seconds: 2), () {
+      _btnController.success();
+    });
+    Timer(const Duration(seconds: 3), () {
+      _btnController.reset();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Padding(padding: EdgeInsets.all(8)),
+          const Text("Movie Search",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+          const Padding(padding: EdgeInsets.all(8)),
+          SizedBox(
+            width: 300,
+            child: TextField(
+              controller: textSearch,
+              decoration: InputDecoration(
+                  hintText: 'Movie Title',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0))),
+            ),
+          ),
+          const Padding(padding: EdgeInsets.all(8)),
+          SizedBox(
+            width: 180,
+            child: RoundedLoadingButton(
+              color: Colors.purple,
+              controller: _btnController,
+              onPressed: () {
+                showAlertDialog(context);
+              },
+              child: const Text('Search',
+                  style: TextStyle(color: Colors.white, fontSize: 16)),
+            ),
+          ),
+          const Padding(padding: EdgeInsets.all(8)),
+          Text(desc,
+              style:
+                  const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          Image.network(imageUrl),
+        ],
+      ),
+    ));
+  }
+
+  showAlertDialog(BuildContext context) {
+    // set up the cancel and confirm buttons
+    Widget cancelButton = TextButton(
+      child: const Text("Cancel"),
+      onPressed: () {
+        _btnController.reset();
+        Navigator.pop(context);
+      },
+    );
+    Widget confirmButton = TextButton(
+      child: const Text("Confirm"),
+      onPressed: () {
+        Navigator.pop(context);
+        _buttonLoading();
+        _getMovie();
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: const Text("Confirmation Alert"),
+      content: const Text("Do you confirm to search the movie?"),
+      actions: [
+        cancelButton,
+        confirmButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  Future<void> _getMovie() async {
+    String search = textSearch.text;
+    var apikey = "b51cd3c0";
+    var url = Uri.parse('https://www.omdbapi.com/?t=$search&apikey=$apikey');
+    var response = await http.get(url);
+    var rescode = response.statusCode;
+    if (rescode == 200) {
+      var jsonData = response.body;
+      var parsedJson = json.decode(jsonData);
+      setState(() {
+        movieTitle = parsedJson['Title'];
+        year = parsedJson['Year'];
+        genre = parsedJson['Genre'];
+        imageUrl = parsedJson['Poster'];
+        desc =
+            "Movie Title:  $movieTitle\n\n Year:  $year\n\n Genre:  $genre \n";
+      });
+    }
+  }
+}
